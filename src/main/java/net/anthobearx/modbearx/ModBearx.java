48c927dd8cacddf47e1e1ee1e6f7d10aca40cc19
anthobearx <anthobearmc@gmail.com>
2023-04-02 12:23:55 -0700

    Clase items awa

--- src/main/java/net/anthobearx/modbearx/ModBearx.java
+++ src/main/java/net/anthobearx/modbearx/ModBearx.java
@@ -1,6 +1,11 @@
 package net.anthobearx.modbearx;
 
+import net.anthobearx.modbearx.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.RegistryWorldView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +14,12 @@ public class ModBearx implements ModInitializer {
 	public static final String MOD_ID = "modbearx";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	/**private static Item registerItem(String nombre, Item item){
+		return Registry.register(Item,)
+	}*/
 	@Override
 	public void onInitialize() {
 
-		// LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/anthobearx/modbearx/item/ModItems.java
@@ -0,0 +1,9 @@
+package net.anthobearx.modbearx.item;
+
+import net.anthobearx.modbearx.ModBearx;
+
+public class ModItems {
+    public static void registerModItems(){
+        ModBearx.LOGGER.debug("Nuevo item mod registrado en " + ModBearx.MOD_ID);
+    }
+}
